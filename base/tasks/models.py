00bb896d4a457be6c0219049f60b122c1a7474e1
masoud533 <mapar10278@gmail.com>
2026-02-12 17:22:36 +0330

    create Task model ref #6

--- base/tasks/models.py
+++ base/tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(
+        'accounts.User',
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+    title = models.CharField(max_length=255)
+    is_done = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
