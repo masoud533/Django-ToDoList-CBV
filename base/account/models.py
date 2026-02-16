d57e74434d8a31794f8070427df53f8d1450a71d
masoud533 <mapar10278@gmail.com>
2026-02-16 13:26:20 +0330

    fix: add custom UserManager for email-only authentication

--- base/account/models.py
+++ base/account/models.py
@@ -1,5 +1,38 @@
 from django.db import models
-from django.contrib.auth.models import (AbstractUser)
+from django.contrib.auth.models import (AbstractUser, BaseUserManager)
+
+class UserManager(BaseUserManager):
+    '''
+    Cutom user model manager where email is the unique identifiers
+    for authenticaion instead of usernames
+    '''
+    def create_user(self,email,password, **extra_fields):
+        """
+        Create and save a User with the given email and password and extra data
+        """
+        if not email:
+            raise ValueError(_("the Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self,email,password,**extra_fields):
+        '''
+        Create and save a SuperUser with the given email and password.
+        '''
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_active',True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_("Superuser must have is_staff=True"))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        
+        return self.create_user(email,password,**extra_fields)
+
 
 class User(AbstractUser):
     username = None
@@ -11,6 +44,7 @@ class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    objects = UserManager()
     def __str__(self):
         return self.email
 
