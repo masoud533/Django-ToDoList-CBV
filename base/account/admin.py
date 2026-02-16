0205dfb7886fd74ec0ea4f9eeee7c4d97504fd3a
masoud533 <mapar10278@gmail.com>
2026-02-16 13:40:56 +0330

    feat: customize Django admin for email-based User model close #7

--- base/account/admin.py
+++ base/account/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', "is_superuser", "is_active")
+    list_filter = ('email', "is_superuser", "is_active")
+    search_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        (None, {
+            "fields": (
+                'email', 'password'
+            ),
+        }),
+        ('permissions', {
+            "fields": (
+                "is_staff", 'is_active', 'is_superuser'
+            ),
+        }),
+        ('group permissions', {
+            "fields": (
+                "groups", 'user_permissions',
+            ),
+        }),
+        ('important date', {
+            "fields": (
+                "last_login",
+            ),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_superuser', 'is_active')
+        }),
+    )
+
+admin.site.register(User,CustomUserAdmin)
