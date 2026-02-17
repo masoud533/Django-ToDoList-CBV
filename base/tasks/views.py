10345e63a91c5b40a47c243be2dde90b8bce7214
masoud533 <mapar10278@gmail.com>
2026-02-17 17:33:55 +0330

    modified tasks views ref #11

--- base/tasks/views.py
+++ base/tasks/views.py
@@ -1,12 +1,12 @@
 # tasks/views.py
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView, DeleteView
 from django.views.generic.edit import FormMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-
+from django.shortcuts import get_object_or_404, redirect
 from .models import Task
 from .forms import TaskCreateForm
-
+from django.views import View
 
 class TaskListView(LoginRequiredMixin, FormMixin, ListView):
     model = Task
@@ -31,3 +31,29 @@ class TaskListView(LoginRequiredMixin, FormMixin, ListView):
             task.save()
         return super().get(request, *args, **kwargs)
 
+
+
+class TaskToggleDoneView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        task = get_object_or_404(Task, pk=pk, user=request.user)
+        task.is_done = not task.is_done
+        task.save()
+        return redirect('tasks:list')
+    
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = ('title',)
+    template_name = 'tasks/task_edit.html'
+    success_url = reverse_lazy('tasks:list')
+
+    def get_queryset(self):
+        # امنیت: فقط تسک‌های خود یوزر
+        return Task.objects.filter(user=self.request.user)
+    
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
+    success_url = reverse_lazy('tasks:list')
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
